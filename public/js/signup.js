0f540eb9d7e1da1e7ea90d94669c2e3ced370691
Taz Whitaker <tazwhitaker@Tazs-MacBook-Air.local>
2023-12-04 21:34:45 -0500

    updated signup handlebar

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,24 +1,34 @@
 const signupFormHandler = async (event) => {
-    event.preventDefault();
+    try {
+        event.preventDefault();
 
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+        const username = document.querySelector('#username-signup').value.trim();
+        const email = document.querySelector('#email-signup').value.trim();
+        const password = document.querySelector('#password-signup').value.trim();
 
-    if (username && email && password) {
-        const response = await fetch('/api/users', {
+        // Additional client-side form validation
+        if (!username || !email || !password) {
+            // Display a user-friendly error message
+            alert('Please fill in all the fields.');
+            return;
+        }
+
+        // Display a loading spinner or indicator
+
+        const response = await fetch('/api/user', {
             method: 'POST',
             body: JSON.stringify({ username, email, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
         if (response.ok) {
+            // Redirect the user to another location upon successful registration
             document.location.replace('/');
         } else {
-            alert(response.statusText);
+            // Display an error message
+            alert(`Error: ${await response.text()}`);
         }
-        
+    } catch (error) {
+        console.error('An error occurred during form submission:', error);
     }
 };
-
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
