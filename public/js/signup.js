9a796881a7ebe2f27464731b7202d3a8c8c86dc6
MadChazo <141524368+MadChazo@users.noreply.github.com>
2023-12-05 21:16:29 -0500

    Worked on bugs in signup process

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,34 +1,37 @@
 const signupFormHandler = async (event) => {
-    try {
-        event.preventDefault();
+  try {
+    event.preventDefault();
 
-        const username = document.querySelector('#username-signup').value.trim();
-        const email = document.querySelector('#email-signup').value.trim();
-        const password = document.querySelector('#password-signup').value.trim();
+    const username = document.querySelector("#username-signup").value.trim();
+    const email = document.querySelector("#email-signup").value.trim();
+    const password = document.querySelector("#password-signup").value.trim();
 
-        // Additional client-side form validation
-        if (!username || !email || !password) {
-            // Display a user-friendly error message
-            alert('Please fill in all the fields.');
-            return;
-        }
-
-        // Display a loading spinner or indicator
-
-        const response = await fetch('/api/user', {
-            method: 'POST',
-            body: JSON.stringify({ username, email, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-
-        if (response.ok) {
-            // Redirect the user to another location upon successful registration
-            document.location.replace('/');
-        } else {
-            // Display an error message
-            alert(`Error: ${await response.text()}`);
-        }
-    } catch (error) {
-        console.error('An error occurred during form submission:', error);
+    // NOTE: We should be using modals, not alerts
+    // Additional client-side form validation
+    if (!username || !email || !password) {
+      // Display a user-friendly error message
+      alert("Please fill in all the fields.");
+      return;
     }
+
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ username, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      // Redirect the user to another location upon successful registration
+      document.location.replace("/");
+    } else {
+      // Display an error message
+      alert(`Error: ${await response.text()}`);
+    }
+  } catch (error) {
+    console.error("An error occurred during form submission:", error);
+  }
 };
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
