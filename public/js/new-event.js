b4aec2858e074cbdca577fe30d39aaf0a8b8e61a
Madeline Urbanic <madelinerurbanic@gmail.com>
2023-12-07 16:47:46 -0500

    get dropdown working and logging to db

--- public/js/new-event.js
+++ public/js/new-event.js
@@ -1,21 +1,6 @@
 async function newFormHandler(event) {
     event.preventDefault();
 
-   /* try {
-        // Fetch the user ID from the server (stored in the session)
-        const response = await fetch("/api/user", {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-        });
-
-        if (!response.ok) {
-        console.error('Failed to fetch user information');
-        return;
-        }
-
-        const userData = await response.json();
-        const user_id = userData.id;
-        */
 
         const title = document.querySelector('#event_title').value;
         const description = document.querySelector('#event_description').value;
@@ -27,27 +12,23 @@ async function newFormHandler(event) {
         const end_date_input = document.getElementById('endtime');
         // make end time a date datatype
         const end_date = end_date_input.value;
+        const dropdown_input = document.getElementById('category_options');
+        // make end time a date datatype
+        const category_id = dropdown_input.value;
 
         const eventResponse = await fetch("/api/events", {
             method: "POST",
-            body: JSON.stringify({ title, description, location, start_date, end_date, category_id: '1' }),
+            body: JSON.stringify({ title, description, location, start_date, end_date, category_id }),
             headers: { "Content-Type": "application/json" },
         });
         
         if (eventResponse.ok) {
             document.location.replace('/');
-            alert(test)
+            alert(category_id)
         } else {
                 alert('Failed to add new event');
         }
 
-/*
-    await fetch('/api/events',{
-        method: "POST",
-        body: {"hello"},
-        headers: { "Content-Type": "application/json" },
-    })
-    */
 }
 
   
